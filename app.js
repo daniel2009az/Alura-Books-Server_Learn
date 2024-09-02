67a228fab624064b979dbf28094dae272224932f
daniel2009az <daniel2009az@gmail.com>
2024-09-02 05:11:29 -0300

    feat: <search> connect api back-end

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express'); // Importando o express
+const express = require('express'); // Importando o express responsavel por criar o servidor
 const books = require('./routes/book'); // Importando o arquivo book.js
-
+const  cors = require('cors'); // Importando o cors
 
 const app = express(); // Inicializando o express
 app.use(express.json()); // Habilitando o uso de JSON
-
+app.use(cors({origin: 'http://localhost:3000'})); // Habilitando o uso do cors
 const port = 8000; // Definindo a porta
 
 app.use('/books', books)// Definindo a rota /books
